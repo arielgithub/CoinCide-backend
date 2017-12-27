6de017d995ceb325a3b5e35a1a3e9351c63f2562
Ariel Esteban Solis <xlin01@gmail.com>
2017-12-27 18:04:52 +0100

    Integrazione del connettore al database mySql e scrittura del DB in base al cambiamento del model ( funzione di automigrazione ).

--- CoinCide-backend-git/server/boot/root.js
+++ CoinCide-backend-git/server/boot/root.js
@@ -1,8 +1,19 @@
 'use strict';
 
-module.exports = function(server) {
+module.exports = function (server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   router.get('/', server.loopback.status());
   server.use(router);
+
+
+
+  var ds = server.dataSources.coinCide;
+  var lbTables = ['Utente'];
+  ds.automigrate(lbTables, function (er) {
+    if (er) throw er;
+    console.log('Loopback tables [' - lbTables - '] created in ', ds.adapter.name);
+    ds.disconnect();
+  });
+
 };
