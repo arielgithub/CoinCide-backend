99fa7f23739187aebe42c17003b3ea950f95eb85
Ariel Esteban Solis <xlin01@gmail.com>
2017-12-29 15:28:16 +0100

    Integration pagination ads

--- common/models/ads.js
+++ common/models/ads.js
@@ -1,5 +1,54 @@
 'use strict';
 
-module.exports = function(Ads) {
+module.exports = function (Ads) {
+
+
+    Ads.getAdsPag = function (pagina, numElementPerPage, cb) {
+        var ds = Ads.dataSource;
+        var offset = (pagina - 1) * numElementPerPage;
+        var sql = "SELECT * FROM ads LIMIT " + numElementPerPage + " OFFSET " + offset;
+        var params = [pagina, numElementPerPage];
+
+        ds.connector.query(sql, params, function (err, ads) {
+
+            if (err) console.error(err);
+
+            cb(err, ads);
+
+        });
+    }
+
+    Ads.remoteMethod(
+        'getAdsPag',
+        {
+            http: { path: '/getAdsPag', verb: 'get' },
+            accepts: [{ arg: 'pagina', type: 'number' }, { arg: 'numElementPerPage', type: 'number' }],
+            returns: { arg: 'data', type: ['Ads'], root: true }
+        }
+    );
+
+
+    Ads.countAdsPag = function (numElementPerPage, cb) {
+        var ds = Ads.dataSource;
+        var sql = "SELECT count(*) AS total, FLOOR((count(*)/"+numElementPerPage+")) AS pages FROM ads";
+
+        ds.connector.query(sql, function (err, ads) {
+
+            if (err) console.error(err);
+
+            cb(err, ads[0]);
+
+        });
+    }
+
+    Ads.remoteMethod(
+        'countAdsPag',
+        {
+            http: { path: '/countAdsPag', verb: 'get' },
+            accepts: [{ arg: 'numElementPerPage', type: 'number' }],
+            returns: { arg: 'data', type: 'Ads', root: true }
+        }
+    );
+
 
 };
